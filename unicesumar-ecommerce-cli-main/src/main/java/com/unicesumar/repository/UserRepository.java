969e8468222118ddc48dc8cbd06aba83227edfbb
Gabrielle Morente Perna <146737057+GabsMorente@users.noreply.github.com>
2025-04-04 22:23:24 -0300

    Update UserRepository.java

--- unicesumar-ecommerce-cli-main/src/main/java/com/unicesumar/repository/UserRepository.java
+++ unicesumar-ecommerce-cli-main/src/main/java/com/unicesumar/repository/UserRepository.java
@@ -1,96 +1,56 @@
-package com.unicesumar.repository;
+package com.unicesumar.entities;
 
-import com.unicesumar.entities.Product;
-import com.unicesumar.entities.Usuario;
-
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-public class UserRepository implements EntityRepository<Usuario>{
-    private final Connection connection;
+public class User extends Entity {
+    private String name;
+    private String email;
+    private String password;
 
-    public UserRepository(Connection connection) {
-        this.connection = connection;
+    public User(UUID uuid, String name, String email, String password) {
+        super(uuid);
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+    public User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
     }
 
     @Override
-    public void save(Usuario entity) {
-        String query = "INSERT INTO users VALUES (?, ?, ?)";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setString(1, entity.getUuid().toString());
-            stmt.setString(2, entity.getName());
-            stmt.setString(3, entity.getEmail());
-            stmt.setString(4, entity.getPassword());
-
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public Optional<Usuario> findById(UUID id) {
-        String query = "SELECT * FROM users WHERE uuid = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setString(1, id.toString());
-            ResultSet resultSet = stmt.executeQuery();
-
-            if (resultSet.next()) {
-                Usuario usuario = new Usuario(
-                        UUID.fromString(resultSet.getString("uuid")),
-                        resultSet.getString("name"),
-                        resultSet.getString("email"),
-                        resultSet.getString("password")
-                );
-                return Optional.of(usuario);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
-        return Optional.empty();
-    }
-
-    @Override
-    public List<Usuario> findAll() {
-        String query = "SELECT * FROM users";
-        List<Usuario> usuarios = new LinkedList<>();
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            ResultSet resultSet = stmt.executeQuery();
-
-            while (resultSet.next()) {
-                Usuario usuario = new Usuario(
-                        UUID.fromString(resultSet.getString("uuid")),
-                        resultSet.getString("name"),
-                        resultSet.getString("email"),
-                        resultSet.getString("password")
-                );
-                usuarios.add(usuario);
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return usuarios;
-    }
-
-    @Override
-    public void deleteById(UUID id) {
-        String query = "DELETE FROM users WHERE uuid = ?";
-        try {
-            PreparedStatement stmt = this.connection.prepareStatement(query);
-            stmt.setString(1, id.toString());
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
+    public String toString() {
+        return "User{" +
+                "uuid=" + getUuid() + // Use inherited getUuid()
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                // Avoid printing password directly in production logs
+                ", password='********'" +
+                '}';
     }
 }
